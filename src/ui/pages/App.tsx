b27ae87838d59078f331a2694926edf2db6876ab
ddigulio <dario.digulio@mercadolibre.com>
2022-10-09 20:36:42 -0300

    Banner component

--- src/ui/pages/App.tsx
+++ src/ui/pages/App.tsx
@@ -1,40 +1,13 @@
 import React from 'react'
 import styled from 'styled-components'
 import { styles } from '../styles'
+import { Banner } from './Home/Banner';
 
 function App() {
   return (
-    <Banner>
-      <Title>Bienestar Estudiantil - Psicología UBA</Title>
-      <Login>Iniciar Sesión</Login>
-    </Banner>
+    <Banner />
   );
 }
 
-const Banner = styled.div`
-  display: flex;
-  position: sticky;
-  background-color: ${styles.colors.primary};
-  padding: 0.25em 1em;
-  border-radius: 10px;
-  justify-content: space-between;
-  box-shadow: 0px 3px 8px 0px #454545bf;
-`;
-
-const Title = styled.h1`
-  color: ${styles.colors.white};
-  font-size: x-large;
-  font-weight: 500;
-`;
-
-const Login = styled.button`
-  border-radius: 20px;
-  padding: 10px;
-  border: none;
-  height: fit-content;
-  align-self: center;
-  color: ${styles.colors.white};
-  background-color: ${styles.colors.alternative};
-`;
 
 export default App;
---
+++ src/ui/pages/Home/Banner.tsx
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+import { styles } from "../../styles";
+
+export const Banner: React.FC = () => {
+    return (
+        <Content>
+            <Title>Bienestar Estudiantil - Psicología UBA</Title>
+            <Login>Iniciar Sesión</Login>
+        </Content>
+    )
+}
+
+const Content = styled.div`
+  display: flex;
+  position: sticky;
+  background-color: ${styles.colors.primary};
+  padding: 0.25em 1em;
+  border-radius: 10px;
+  justify-content: space-between;
+  box-shadow: 0px 3px 8px 0px #454545bf;
+`;
+
+const Title = styled.h1`
+  color: ${styles.colors.white};
+  font-size: x-large;
+  font-weight: 500;
+`;
+
+const Login = styled.button`
+  border-radius: 20px;
+  padding: 10px;
+  border: none;
+  height: fit-content;
+  align-self: center;
+  color: ${styles.colors.white};
+  background-color: ${styles.colors.alternative};
+`;
